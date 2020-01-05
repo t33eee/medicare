52c8eab621a50db1ac884a7cc2f35f1226e119b6
kemadu <53985232+kemadu@users.noreply.github.com>
2020-01-05 02:42:12 +0100

    Updates

---
+++ precache-manifest.203a93fa9baeaefb9124b86277727088.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "610b950a19e5bdffeb2a10667c3437b2",
+    "url": "/medicare/index.html"
+  },
+  {
+    "revision": "c393d02b7cb4a0271a1b",
+    "url": "/medicare/static/css/main.d9bcd6a8.chunk.css"
+  },
+  {
+    "revision": "11ca25e43ad27dd2f93c",
+    "url": "/medicare/static/js/2.d81be70a.chunk.js"
+  },
+  {
+    "revision": "63081033f0715f1417b5b2a63b591bb6",
+    "url": "/medicare/static/js/2.d81be70a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c393d02b7cb4a0271a1b",
+    "url": "/medicare/static/js/main.aa5bbfdc.chunk.js"
+  },
+  {
+    "revision": "9036e787589bd47337d6",
+    "url": "/medicare/static/js/runtime-main.56d9eff1.js"
+  },
+  {
+    "revision": "692534c768c2266d6a86fac7d9aa1836",
+    "url": "/medicare/static/media/dedicated-doctors.692534c7.jpg"
+  },
+  {
+    "revision": "fa3afb888ad9b8bebcb697ed1a5c7eb1",
+    "url": "/medicare/static/media/dentist.fa3afb88.jpg"
+  },
+  {
+    "revision": "7879b116bf9673f13ecce7625465c2ea",
+    "url": "/medicare/static/media/doctor1.7879b116.jpg"
+  },
+  {
+    "revision": "96790efdf13e15521ff77743c8cf7073",
+    "url": "/medicare/static/media/doctor2.96790efd.jpg"
+  },
+  {
+    "revision": "14753287a1f1c4329f5d5771243392af",
+    "url": "/medicare/static/media/doctor3.14753287.jpg"
+  },
+  {
+    "revision": "25a1c275bd423f8fc11917039cdd46ae",
+    "url": "/medicare/static/media/doctors-intro.25a1c275.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/medicare/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/medicare/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/medicare/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/medicare/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/medicare/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "4a00b5297ff09769eea3c5fd4c016e7f",
+    "url": "/medicare/static/media/header.4a00b529.jpg"
+  },
+  {
+    "revision": "c5c489f41315bbe69f4ce9077ac32592",
+    "url": "/medicare/static/media/logo.c5c489f4.png"
+  },
+  {
+    "revision": "baf9580566f072cc2d5e952a62e76ceb",
+    "url": "/medicare/static/media/man-next.baf95805.jpg"
+  },
+  {
+    "revision": "ea7d8be40642f32d60e04f06f1492f17",
+    "url": "/medicare/static/media/meeting.ea7d8be4.jpg"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/medicare/static/media/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/medicare/static/media/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/medicare/static/media/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/medicare/static/media/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/medicare/static/media/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "fa928246f437313680edefd57d0c17cb",
+    "url": "/medicare/static/media/nurse-patient.fa928246.jpg"
+  },
+  {
+    "revision": "682dc851fd9beb9da0cd480994293c71",
+    "url": "/medicare/static/media/nurse-patient2.682dc851.jpg"
+  },
+  {
+    "revision": "d7520f7b2455b764d72651a9e945e01b",
+    "url": "/medicare/static/media/nurse-patient3.d7520f7b.jpg"
+  },
+  {
+    "revision": "79586c4a05fef1ea1ac3cae99661268c",
+    "url": "/medicare/static/media/op-facility.79586c4a.jpg"
+  },
+  {
+    "revision": "0a08a03428c8080c54e5ba3aa8f73d78",
+    "url": "/medicare/static/media/sugery.0a08a034.jpg"
+  }
+]);
